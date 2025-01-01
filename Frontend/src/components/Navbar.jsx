12c6a78902a3e5aaca436d9c85e5adc76f4fdd40
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2025-01-01 11:27:09 +0300

    start woking on Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="">
+      <div className="flex items-center justify-evenly h-12 bg-black pl-10">
+        <p className="text-sm text-[#FAFAFA]">Gena Festival Special: Unlock Amazing Deals on House Rentals and Purchases! 🎉 Enjoy Exclusive Discounts and Flexible Pricing Options! Don't Miss Out<a href="#" className="font-semibold underline ml-2">Explore Now!</a></p>
+        <select className="bg-black text-[#FAFAFA] ml-10">
+          <option value="English">English</option>
+          <option value="Amharic">Amharic</option>
+        </select>
+      </div>
+      </div>
+    </div>
   )
 }
 export default Navbar
